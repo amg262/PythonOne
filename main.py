1a5488b8d8defa5142097c65d2370d15c71f0050
Andrew <andrewmgunn26@gmail.com>
2021-11-03 19:22:26 -0500

    def get_age def go functions working to get input

--- main.py
+++ main.py
@@ -4,13 +4,24 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+# def print_hi(name):
+#     # Use a breakpoint in the code line below to debug your script.
+#     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+#
+#
+# # Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+#     print_hi('PyCharm')
+
+def get_age():
+    age = int(input("Age:"))
+    print(age)
 
 
-# Press the green button in the gutter to run the script.
+def go():
+    print(f'Hey', {get_age()})
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
+    get_age()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
