24b71d0c54c5110822fb16d1e8dffc8434339058
Andrew <andrewmgunn26@gmail.com>
2021-11-17 17:39:44 -0600

    finish while.py while loop file

--- while.py
+++ while.py
@@ -1,13 +1,19 @@
-
-steps = int(input("Steps: (-1 to exit)"))
-net = 0
-count = 1
-
-
-while steps != -1:
-    steps = int(input(f"Steps Day {count}: (-1 to exit)"))
+def go_while():
+    count = 0
+    net = 0
+    steps = int(input(f"Steps Day 1 (-1 to exit)"))
     net += steps
+
     count += 1
+    while steps != -1:
+        steps = int(input(f"Steps Day {count + 1}: (-1 to exit)"))
+        net += steps
+        count += 1
 
-print(f"Days: {count} - Steps: {net} - Avg {net / count}:.2f")
+    count -= 1
+    net += 1
 
+    print(f"Days: {count} - Steps: {net} - Avg {net / count}")
+
+
+go_while()
